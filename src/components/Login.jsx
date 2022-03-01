31877d658710aa8b6e1d1212263f9813e225a6b3
kuldeepsingh1828 <kuldeepsingh1828@gmail.com>
2022-03-01 22:46:40 +0530

    commit to add state in the function component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,12 +1,62 @@
+import { useState } from 'react';
 function Login({ color, text }) {
-    color = "yellow";
-    return <div>
-        <label htmlFor="">Enter Your E-Mail</label><br />
-        <input type="text" name="" id="" placeholder="Email here" /><br />
-        <label htmlFor="">Enter Your Password</label><br />
-        <input type="password" name="" id="" placeholder="password" /><br />
-        <input type="submit" value={text} style={{ backgroundColor: color }} /><br />
-        <button onClick={() => { color = "pink"; console.log(color); }}>click me to change the color</button>
-    </div >
+
+    const [data, setData] = useState({
+        name: '',
+        password: '',
+        cpassword: '',
+        email: '',
+        hidden: "none"
+    });
+
+    const { name, email, password, cpassword, hidden } = data;
+
+    const changeHandler = function (event) {
+        let field = event.target.name;
+        let value = event.target.value;
+        setData((state) => ({ ...state, [field]: value }));
+    }
+    const submitHandler = function () {
+        setData((state) => ({ ...state, hidden: "" }));
+    }
+    return <>
+        <input
+            type="text"
+            placeholder="enter some name"
+            name="name"
+            value={name}
+            onChange={changeHandler} />
+
+        <input
+            type="text"
+            placeholder="enter some password"
+            name="password"
+            value={password}
+            onChange={changeHandler} />
+
+        <input
+            type="text"
+            placeholder="enter password again"
+            name="cpassword"
+            value={cpassword}
+            onChange={changeHandler} />
+
+        <input
+            type="text"
+            placeholder="enter some email"
+            name="email"
+            value={email}
+            onChange={changeHandler} />
+        <input
+            onClick={submitHandler}
+            type="submit"
+            value="show data"
+        />
+        <div style={{ display: hidden }}>
+            name:{name}<br />
+            Email:{email}<br />
+            Password:{password}<br />
+        </div>
+    </>
 }
 export default Login;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import ReactDOM from 'react-dom';
 import Login from './components/Login';
+import ClassLogin from './components/ClassLogin';
 
 let loginText= "Login ME";
 let loginColor="green";
 let registerText= "Register ME";
 let registerColor="red";
-ReactDOM.render(<div><Login text={loginText} color={loginColor} /><br /><Login color={registerColor} text={registerText} /></div>, document.getElementById("root"));
+ReactDOM.render(<Login/>, document.getElementById("root"));
